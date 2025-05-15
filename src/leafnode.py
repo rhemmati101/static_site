de4d1230f95c3cfea27d1e0a9d8f09f712ff72b2
rhemmati101 <rhemmati@g.ucla.edu>
2025-05-14 18:55:02 -0700

    add text node to leaf node func, rename plain texttype enum

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,4 +1,6 @@
 from htmlnode import HTMLNode
+from textnode import TextNode
+from textnode import TextType
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
@@ -10,5 +12,22 @@ class LeafNode(HTMLNode):
         if self.tag == None:
             return self.value
         return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+def text_node_to_html_node(text_node: 'TextNode') -> 'LeafNode':
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b",value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i",value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag="code",value=text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag="a",value=text_node.text, props={"href":text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(tag="img", value="", props={"src":text_node.url, "alt":text_node.text})
+        case _:
+            raise Exception(f"{text_node.text_type} is not a supported text type")
         
 
