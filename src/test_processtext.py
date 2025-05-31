7cbfbad8c445541ce1855dbae8a0b6a992e94e75
rhemmati101 <rhemmati@g.ucla.edu>
2025-05-30 18:15:47 -0700

    slightly more testing

--- src/test_processtext.py
+++ src/test_processtext.py
@@ -347,22 +347,39 @@ class TestProcessText(unittest.TestCase):
             new_nodes,
         )
 
-    def test_text_to_textnodes(self):
+    def test_text_to_textnodes_all_types(self):
         text = TextNode("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)", TextType.TEXT)
 
         self.assertEqual(
             text_to_textnodes(text),
             [
-            TextNode("This is ", TextType.TEXT),
-            TextNode("text", TextType.BOLD),
-            TextNode(" with an ", TextType.TEXT),
-            TextNode("italic", TextType.ITALIC),
-            TextNode(" word and a ", TextType.TEXT),
-            TextNode("code block", TextType.CODE),
-            TextNode(" and an ", TextType.TEXT),
-            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
-            TextNode(" and a ", TextType.TEXT),
-            TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
+    def test_text_to_textnodes_double_delimiters(self):
+        text = TextNode("This is **text** _with_ **two** _double_ delimiters", TextType.TEXT)
+
+        self.assertEqual(
+            text_to_textnodes(text),
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" ", TextType.TEXT),
+                TextNode("with", TextType.ITALIC),
+                TextNode(" ", TextType.TEXT),
+                TextNode("two", TextType.BOLD),
+                TextNode(" ", TextType.TEXT),
+                TextNode("double", TextType.ITALIC),
+                TextNode(" delimiters", TextType.TEXT),
             ]
         )
 
