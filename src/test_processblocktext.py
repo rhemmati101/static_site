fd776607e87856ea93871e6893e06358e92bd0a7
rhemmati101 <rhemmati@g.ucla.edu>
2025-07-10 12:46:10 -0700

    test and fix markdown to blocks

--- src/test_processblocktext.py
+++ src/test_processblocktext.py
@@ -4,16 +4,72 @@ from processblocktext import markdown_to_blocks
 
 
 class TestProcessBlockText(unittest.TestCase):
+        def test_my_sanity(self):
+             self.assertEqual("test", "\ntest\n".strip())
         def test_markdown_to_blocks(self):
             md = """
-    This is **bolded** paragraph
+This is **bolded** paragraph
 
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
-    - This is a list
-    - with items
-    """
+- This is a list
+- with items
+"""
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(
+                blocks,
+                [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                    "- This is a list\n- with items",
+                ],
+            )
+        def test_markdown_to_blocks_oneblock(self):
+            md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+            md = """
+This is **bolded** paragraph with a lot after it
+
+
+
+"""
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(
+                blocks,
+                ["This is **bolded** paragraph with a lot after it",],
+            )
+        def test_markdown_to_blocks_empty(self):
+            md = """
+
+
+"""
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(
+                blocks,
+                [],
+            )
+        def test_markdown_to_blocks_extraspace(self):
+            md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+
+
+
+
+- This is a list
+- with items
+"""
             blocks = markdown_to_blocks(md)
             self.assertEqual(
                 blocks,
