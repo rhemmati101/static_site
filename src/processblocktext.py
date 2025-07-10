d196285188fa5ba0f2071f4e654ddd66ff84ce5b
rhemmati101 <rhemmati@g.ucla.edu>
2025-07-10 13:53:50 -0700

    block to blocktype + testing!

--- src/processblocktext.py
+++ src/processblocktext.py
@@ -1,3 +1,16 @@
+from enum import Enum
+
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNOLIST = "unordered list"
+    OLIST = "ordered list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks: list[str] = []
     
@@ -7,3 +20,24 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = list(filter(lambda line: line != "", map(lambda text: text.strip(), text_lines)))
 
     return blocks
+
+def block_to_blocktype(block: str) -> BlockType:
+    if re.match(r"^#{1,6} [^\n]*$", block):
+        return BlockType.HEADING
+    
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    if all(line.startswith(">") for line in block.split("\n")):
+        return BlockType.QUOTE
+    
+    if all(line.startswith("- ") for line in block.split("\n")):
+        return BlockType.UNOLIST
+    
+    if all(re.match(r"^\d+\. ", line) for line in block.split("\n")):
+        #oh boy is this convoluted!
+        nums: list[str] = re.findall(r"^(\d+)\. ", block, flags=re.MULTILINE)
+        if all(nums[i] == str(i + 1) for i in range(len(nums))):
+            return BlockType.OLIST
+
+    return BlockType.PARAGRAPH
--- src/test_processblocktext.py
+++ src/test_processblocktext.py
@@ -1,6 +1,6 @@
 import unittest
 
-from processblocktext import markdown_to_blocks
+from processblocktext import markdown_to_blocks, BlockType, block_to_blocktype
 
 
 class TestProcessBlockText(unittest.TestCase):
@@ -78,6 +78,107 @@ This is the same paragraph on a new line
                     "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                     "- This is a list\n- with items",
                 ],
+            )
+
+        def test_block_to_blocktype_simpleheading(self):
+            self.assertEqual(
+                 block_to_blocktype("# this is a basic heading!"),
+                 BlockType.HEADING
+            )
+        def test_block_to_blocktype_tinyheading(self):
+            self.assertEqual(
+                 block_to_blocktype("###### this is a heading with 6 #'s"),
+                 BlockType.HEADING
+            )
+        def test_block_to_blocktype_invalidheading(self):
+            self.assertEqual(
+                 block_to_blocktype("####### this is a heading with 7 #'s"),
+                 BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_emptyheading(self):
+            self.assertEqual(
+                 block_to_blocktype("###  "),
+                 BlockType.HEADING
+            )
+        def test_block_to_blocktype_code(self):
+            self.assertEqual(
+                block_to_blocktype("```\nthis is some code!\n```"),
+                BlockType.CODE
+            )
+        def test_block_to_blocktype_invalidcode1(self):
+            self.assertEqual(
+                block_to_blocktype("``\nthis is an invalid code block\n```"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_invalidcode2(self):
+            self.assertEqual(
+                block_to_blocktype("```\nthis is an invalid code block\n``"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_simplequote(self):
+            self.assertEqual(
+                block_to_blocktype("> I...am Steve."),
+                BlockType.QUOTE
+            )
+        def test_block_to_blocktype_multiquote(self):
+            self.assertEqual(
+                block_to_blocktype("> I...am Steve.\n> applause, I guess"),
+                BlockType.QUOTE
+            )
+        def test_block_to_blocktype_invalidquote(self):
+            self.assertEqual(
+                block_to_blocktype("> I...am Steve.\nthis is invalid!"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_simpleunolist(self):
+            self.assertEqual(
+                block_to_blocktype("- I am a lonely single-line list :("),
+                BlockType.UNOLIST
+            )
+        def test_block_to_blocktype_multiunolist(self):
+            self.assertEqual(
+                block_to_blocktype("- I have a friend\n- I'm the friend"),
+                BlockType.UNOLIST
+            )
+        def test_block_to_blocktype_invalidunolist(self):
+            self.assertEqual(
+                block_to_blocktype("- I am a lonely single-line list :(\nnot even a list sadge"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_nospaceunolist(self):
+            self.assertEqual(
+                block_to_blocktype("- I am a lonely single-line list :(\n-not even a list sadge"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_simpleolist(self):
+            self.assertEqual(
+                block_to_blocktype("1. I am alone ._."),
+                BlockType.OLIST
+            )
+        def test_block_to_blocktype_multiolist(self):
+            self.assertEqual(
+                block_to_blocktype("1. I\n2. am\n3. a\n4. list?"),
+                BlockType.OLIST
+            )
+        def test_block_to_blocktype_nospaceolist(self):
+            self.assertEqual(
+                block_to_blocktype("1. I\n2. am\n3.not a\n4. list"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_nonascendingolist(self):
+            self.assertEqual(
+                block_to_blocktype("1. I\n2. am\n5. not a\n4. list"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_badstartolist(self):
+            self.assertEqual(
+                block_to_blocktype("2. I\n3. am\n4. not a\n5. list"),
+                BlockType.PARAGRAPH
+            )
+        def test_block_to_blocktype_inconsistentolist(self):
+            self.assertEqual(
+                block_to_blocktype("1. I\n2. am\nnot a\n3. list"),
+                BlockType.PARAGRAPH
             )
 
 
