4f476ac78fb87fc4e3d94fbdfb24ea804e9c6537
rhemmati101 <rhemmati@g.ucla.edu>
2025-07-16 21:22:26 -0700

    starting static site generation journey

---
+++ src/filemanip.py
@@ -0,0 +1,31 @@
+import shutil
+import os
+
+def copy_directory(source_path: str, destination_path: str) -> None:
+    #delete all files in destination directory
+    shutil.rmtree(destination_path)
+    os.mkdir(destination_path)
+
+
+    #copy files from source to destination
+    recursive_copy(source_path, destination_path, "")
+
+def recursive_copy(source_path: str, destination_path: str, extension_from_paths: str) -> None:
+    curr_source_path: str = os.path.join(source_path, extension_from_paths)
+    curr_destination_path: str = os.path.join(destination_path, extension_from_paths)
+    
+    if not (os.path.exists(curr_source_path) and os.path.exists(curr_destination_path)):
+        raise ValueError("invalid path passed as input")
+
+    #assume source path input is a directory
+    if not os.path.isdir(curr_source_path) or not os.path.isdir(curr_destination_path):
+        raise ValueError("path inputs must be directories")
+    
+    contents: list[str] = os.listdir(curr_source_path)
+
+    for c in contents:
+        if os.path.isfile(c):
+            shutil.copy(c, curr_destination_path)
+        elif os.path.isdir(c):
+            os.mkdir(os.path.join(curr_destination_path, c))
+            recursive_copy(source_path, destination_path, os.path.join(extension_from_paths, c))
